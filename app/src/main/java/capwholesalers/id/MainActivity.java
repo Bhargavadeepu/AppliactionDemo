180c105d7748d7afa2436e5ef872679ebff1202d
bhargavadeepu <bhargavaganjam89@gmail.com>
2018-10-18 15:40:58 +0530

    vv

--- app/src/main/java/capwholesalers/id/MainActivity.java
+++ app/src/main/java/capwholesalers/id/MainActivity.java
@@ -17,8 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+          FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
